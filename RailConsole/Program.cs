e1ece9479a85d5c08dd33733db1c92d6909fa995
anttross <anttross@gmail.com>
2015-06-10 20:03:56 +0300

    DAL

--- RailConsole/Program.cs
+++ RailConsole/Program.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Data;
+using RailModels;
 
 namespace RailConsole
 {
@@ -11,9 +14,68 @@ namespace RailConsole
     {
         static void Main(string[] args)
         {
-           RailDAL test = new RailDAL();
-           test.GetAllStations();
+
+
+            List<string> l = new List<string>();
+
+            DataTable allStations = GetRoute("עתלית", "קריית מוצקין");
+
+            //l=test.GetAllStations();
+            //l = test.GetStationsByLine("a");
+
+            foreach (string s in l)
+                Debug.WriteLine(s);
+
+        }
+
+        public static DataTable GetRoute(string arriv, string depart)
+        {
+            string line = null;
+
+            DataTable dtArr = new DataTable();
+            dtArr = RailDAL.GetLineByStation(arriv);
+            DataTable dtDep = new DataTable();
+            dtDep = RailDAL.GetLineByStation(depart);
+            for (int i = 0; i < dtArr.Rows.Count; i++)
+            {
+                for (int j = 0; j < dtDep.Rows.Count; j++)
+                {
+                    if (dtArr.Rows[i][0].ToString() == dtDep.Rows[j][0].ToString())
+                        line = dtArr.Rows[i][0].ToString();
+                }
+            }
+            DataTable dtRes = RailDAL.GetStationsByLine(line);
+            int start = 0, fin = 0;
+
+            for (int i = 0; i < dtRes.Rows.Count; i++)
+            {
+                if (dtRes.Rows[i]["StationName"].ToString() == arriv)
+                    start = Convert.ToInt32(dtRes.Rows[i]["Position"]);
+                if (dtRes.Rows[i]["StationName"].ToString() == depart)
+                    fin = Convert.ToInt32(dtRes.Rows[i]["Position"]);
+            }
+            for (int i = 0; i < dtRes.Rows.Count; i++)
+            {
+                if (Convert.ToInt32(dtRes.Rows[i]["Position"]) < (start < fin ? start : fin))
+                {
+                    dtRes.Rows.Remove(dtRes.Rows[i]);
+                    i--;
+                    continue;
+                }
+                if (Convert.ToInt32(dtRes.Rows[i]["Position"]) > (start < fin ? fin : start))
+                {
+                    dtRes.Rows.Remove(dtRes.Rows[i]);
+                    i--;
+                }
+            }
+            if (start > fin)
+            {
+                DataView dv = dtRes.DefaultView;
+                dv.Sort = "Position desc";
+                dtRes = dv.ToTable();
+            }
+
+            return dtRes;
         }
     }
 }
- 
--- RailModels/RailDAL.cs
+++ RailModels/RailDAL.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using System.Data.SqlClient;
+using System.Data.Sql;
 using System.Diagnostics;
 
 namespace RailModels
@@ -11,24 +13,67 @@ namespace RailModels
     public class RailDAL
     {
         private static string conStr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Ant\Documents\Visual Studio 2013\Projects\RailApp\RailModels\RailDB.mdf;Integrated Security=True;Connect Timeout=30";
-        private SqlConnection connection = new SqlConnection(conStr);
+        private static SqlConnection connection = new SqlConnection(conStr);
 
-        public void GetAllStations()
+        public static DataTable GetAllStations()
+        {
+            return StoredProcedure("GetAllStations", null, null);
+        }
+
+        public static DataTable GetStationsByLine(string line)
+        {
+            return StoredProcedure("GetAllStationsByLine", line, null);
+        }
+
+        public static DataTable GetLineByStation(string station)
+        {
+            return StoredProcedure("GetLineByStation", null, station);
+        }
+
+       
+
+        
+        //private List<string> Query(string query)
+        //{
+        //    List<string> l = new List<string>();
+        //    connection.Open();
+
+
+        //    SqlCommand command = new SqlCommand(query, connection);
+        //    using (SqlDataReader reader = command.ExecuteReader())
+        //    {
+        //        int i = 1;
+        //        while (reader.Read())
+        //        {
+        //            l.Add((string)reader[0]);
+        //            i++;
+        //        }
+
+        //    }
+        //    connection.Close();
+        //    return l;
+        //}
+
+        private static DataTable StoredProcedure(string procName, string line, string station)
         {
             connection.Open();
 
-            SqlCommand command = new SqlCommand("SELECT * FROM Stations", connection);
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                int i = 1;
-                while(reader.Read())
-                {
-                    Debug.WriteLine(i + "\t" + reader[0]);
-                    i++;
-                }
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.Connection = connection;
 
-            }
+            command.CommandText = procName;
+            if(line !=null)
+                command.Parameters.AddWithValue("@Line", line);
+            if (station != null)
+                command.Parameters.AddWithValue("@Station", station);
+            SqlDataReader reader = command.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+            
             connection.Close();
+            return dt;
         }
+
     }
 }
